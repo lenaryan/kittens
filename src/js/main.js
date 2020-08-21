78a459df4e0f74d44fef5c2a7434179a5357a3f6
Lena Ryan <alissadyiphil@gmail.com>
2020-08-22 00:44:09 +0300

    scripts for the price block

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,91 @@
 document.addEventListener('DOMContentLoaded', function() {
     let squareElems;
     let equipElems;
+    let minPrice = document.querySelector('.filter__input--min');
+    let maxPrice = document.querySelector('.filter__input--max');
     let rooms = document.querySelectorAll('.room');
     let resetBtn = document.querySelector('.reset-btn');
 
     document.querySelector('.apply-btn').addEventListener('click', () => {
-        let checkedSquare = [];
+        rooms.forEach(room => {
+            room.classList.remove("show-room");
+            if (!room.classList.contains("hide-room")) {
+                room.classList.add("hide-room");
+                room.style.display = "none";
+            }
+        })
 
         squareElems = document.querySelectorAll('.filter__block--square .filter__check:checked');
         equipElems = document.querySelectorAll('.filter__block--equip .filter__check:checked');
 
-
-        if (squareElems.length || equipElems.length) {
-            resetBtn.style.display = 'block';
+        //selected square part only
+        if (squareElems.length && !equipElems.length && !minPrice.value && !maxPrice.value) {
+            let checkedSquare = [];
 
             //get an array of selected squares
-            squareElems.forEach(elem => checkedSquare.push(elem.dataset.square))
-            
+            squareElems.forEach(elem => checkedSquare.push(elem.dataset.square));
+
             rooms.forEach(room => {
-                room.classList.remove("show-room");
-                if (!room.classList.contains("hide-room")) {
-                    room.classList.add("hide-room");
-                    room.style.display = "none";
-                }
+                // room.classList.remove("show-room");
+                // if (!room.classList.contains("hide-room")) {
+                //     room.classList.add("hide-room");
+                //     room.style.display = "none";
+                // }
                 if (checkedSquare.includes(room.dataset.square)) {
                     room.style.display = "flex";
                     room.classList.add('show-room');
                     room.classList.remove('hide-room');
-                } 
+                }  
             })
+        }
+
+        //filled in price part only
+        if ((minPrice.value || maxPrice.value) && !squareElems.length && !equipElems.length) {
+            if (minPrice.value && maxPrice.value) {
+                rooms.forEach(room => {
+                    if ((room.dataset.price < minPrice.value) || (room.dataset.price > maxPrice.value)) {
+                        room.classList.add("hide-room");
+                        room.classList.remove('show-room');
+                        room.style.display = "none";
+                    } else {
+                        room.classList.remove("hide-room");
+                        room.classList.add('show-room');
+                        room.style.display = "flex";
+                    }
+                })
+            } else if (minPrice.value) {
+                rooms.forEach(room => {
+                    if (room.dataset.price < minPrice.value) {
+                        room.classList.add("hide-room");
+                        room.classList.remove('show-room');
+                        room.style.display = "none";
+                    } else {
+                        room.classList.remove("hide-room");
+                        room.classList.add('show-room');
+                        room.style.display = "flex";
+                    }
+                })
+            } else if (maxPrice.value) {
+                rooms.forEach(room => {
+                    if (room.dataset.price > maxPrice.value) {
+                        room.classList.add("hide-room");
+                        room.classList.remove('show-room');
+                        room.style.display = "none";
+                    } else {
+                        room.classList.remove("hide-room");
+                        room.classList.add('show-room');
+                        room.style.display = "flex";
+                    }
+                })
+            }
+        }
+
+
+        if (squareElems.length || equipElems.length || minPrice.value || maxPrice.value) {
+            resetBtn.style.display = 'block';
         } else {
             //show rooms and hide the reset button
             //if filters are empty
-
             resetBtn.style.display = 'none';
             rooms.forEach(room => {
                 room.classList.remove("hide-room");
@@ -40,7 +93,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 room.removeAttribute("style");
             })
         }
-
     })
 
     resetBtn.addEventListener('click', () => {
