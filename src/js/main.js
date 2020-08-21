eb1b437eb3e481d8a8d4b9aa5b5b7f028bf30d77
Lena Ryan <alissadyiphil@gmail.com>
2020-08-21 23:14:32 +0300

    added the reset button behaviour

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let squareElems;
     let equipElems;
     let rooms = document.querySelectorAll('.room');
+    let resetBtn = document.querySelector('.reset-btn');
 
     document.querySelector('.apply-btn').addEventListener('click', () => {
         let checkedSquare = [];
@@ -9,21 +10,48 @@ document.addEventListener('DOMContentLoaded', function() {
         squareElems = document.querySelectorAll('.filter__block--square .filter__check:checked');
         equipElems = document.querySelectorAll('.filter__block--equip .filter__check:checked');
 
-        squareElems.forEach(elem => checkedSquare.push(elem.dataset.square))
-        
-        rooms.forEach(room => {
-            room.classList.remove("show-room");
-            if (!room.classList.contains("hide-room")) {
-                room.classList.add("hide-room");
-                room.style.display = "none";
-            }
-            if (checkedSquare.includes(room.dataset.square)) {
-                room.style.display = "block";
-                room.classList.add('show-room');
-                room.classList.remove('hide-room');
-            } 
-        })
+
+        if (squareElems.length || equipElems.length) {
+            resetBtn.style.display = 'block';
+
+            //get an array of selected squares
+            squareElems.forEach(elem => checkedSquare.push(elem.dataset.square))
+            
+            rooms.forEach(room => {
+                room.classList.remove("show-room");
+                if (!room.classList.contains("hide-room")) {
+                    room.classList.add("hide-room");
+                    room.style.display = "none";
+                }
+                if (checkedSquare.includes(room.dataset.square)) {
+                    room.style.display = "flex";
+                    room.classList.add('show-room');
+                    room.classList.remove('hide-room');
+                } 
+            })
+        } else {
+            //show rooms and hide the reset button
+            //if filters are empty
+
+            resetBtn.style.display = 'none';
+            rooms.forEach(room => {
+                room.classList.remove("hide-room");
+                room.classList.remove("show-room");
+                room.removeAttribute("style");
+            })
+        }
+
     })
 
-    //если фильтр пустой???
+    resetBtn.addEventListener('click', () => {
+        rooms.forEach(room => {
+            room.classList.remove("hide-room");
+            room.classList.remove("show-room");
+            room.removeAttribute("style");
+        })
+
+        document.querySelectorAll('.filter__check:checked').forEach(checkbox => checkbox.checked = false);
+        document.querySelectorAll('.filter__input').forEach(input => input.value = null);
+        resetBtn.removeAttribute('style');
+    })
 })
